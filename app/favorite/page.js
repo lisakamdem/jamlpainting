68da6fd44a21ad14319654e018d6bb6a384a6db7
lisakamdem <120063552+lisakamdem@users.noreply.github.com>
2023-11-24 12:20:57 -0700

    working on favorites

--- app/favorite/page.js
+++ app/favorite/page.js
@@ -1,21 +1,20 @@
 // FavoritesPage.js
-import React from 'react';
+import FavoritesPage from "./favorites";
 
-export default function FavoritesPage({ favorites, onRemoveFavorite }) {
-
-    // Ensure that favorites is always an array
-  const favoritesList = favorites || [];
-  return (
-    <div>
-      <h2>Favorites</h2>
-      <ul>
-        {favoritesList.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => onRemoveFavorite(item)}>Remove from Favorites</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+export default function Page({ favorites, onRemoveFavorite }) {
+   // Ensure that favorites is always an array
+   const favoritesList = favorites || [];
+   return (
+     <div>
+       <h2>Favorites</h2>
+       <ul>
+         {favoritesList.map((item, index) => (
+           <li key={index}>
+             {item}
+             <button onClick={() => onRemoveFavorite(item)}>Remove from Favorites</button>
+           </li>
+         ))}
+       </ul>
+     </div>
+   );
+ }
